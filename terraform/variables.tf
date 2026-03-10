f035b4e977b234a30f433e0c3db42ce09f3c5821
Albert Moreno <albertmorenovendrell@gmail.com>
2026-03-10 22:40:17 +0100

    fix: remove deprecated quotes from Terraform type constraints

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,16 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources in"
-  type        = "string"
+  type        = string
   default     = "eu-west-1"
 }
 
 variable "domain_name" {
   description = "The domain name for the shopping list app"
-  type        = "string"
+  type        = string
   default     = "list.albertmoreno.dev"
 }
 
 variable "hosted_zone_id" {
   description = "The Route 53 Hosted Zone ID for the domain"
-  type        = "string"
+  type        = string
 }
