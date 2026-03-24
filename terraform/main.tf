d72d1a0c56e23f8875b92f61d7ff27a30dc8bf92
Albert Moreno <albertmorenovendrell@gmail.com>
2026-03-24 22:15:19 +0100

    feat: configure S3 remote backend for Terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket         = "p2p-shopping-list-tf-state"
+    key            = "p2p-shopping-list/terraform.tfstate"
+    region         = "eu-west-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
