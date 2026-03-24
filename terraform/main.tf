5435b9235e04d5edac64fe30bd9b984c308ff037
Albert Moreno <albertmorenovendrell@gmail.com>
2026-03-24 22:21:01 +0100

    fix: set correct S3 backend region to eu-central-1

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "albertmoreno-tf-state"
     key            = "p2p-shopping-list/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-central-1"
   }
 
   required_providers {
